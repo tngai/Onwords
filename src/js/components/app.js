84af461b27d5e1102b0a33c2451c0f2f0f6a8c95
Irving <Irvingb232@gmail.com>
2015-10-19 15:04:33 -0700

    [Progress] Index, main, App in progress

---
+++ src/js/components/app.js
@@ -0,0 +1,17 @@
+var React = require('react');
+
+var Router = require('react-router-component');
+var Locations = Router.Locations;
+var Location = Router.Location;
+
+var App = React.createClass({
+  render: function() {
+    return (
+      <div>
+        HELLO!!
+      </div>
+    );
+  }
+});
+
+module.exports = App;
