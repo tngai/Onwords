ce523c974e4bf0f0e16ff39f3b1bd498a99a1a61
Jihoon Kim <jhk2020@gmail.com>
2015-10-19 11:27:29 -0700

    [feat] create chrome extension using firebase

---
+++ test.js
@@ -0,0 +1,38 @@
+var loadfunction = window.onload;
+
+window.onload = function(event) {
+
+
+  var pageUri = function() {
+    return {
+      beforeAnnotationCreated: function(ann) {
+        ann.uri = window.location.href.split("?")[0];
+      }
+    };
+  };
+
+  var app = new annotator.App();
+  app.include(annotator.ui.main)
+     .include(annotator.storage.http, {
+      prefix: 'https://testing102.firebaseio.com',
+      urls: {
+        create: '/annotations.json',
+        update: '/annotations/{id}.json',
+        destroy: '/annotations/{id}.json',
+        search: '/annotations.json'
+      }
+    })
+     .include(pageUri)
+
+  app.start()
+     .then(function() {
+        app.annotations.load({uri: window.location.href.split("?")[0]});
+     })
+
+  if (loadfunction) {
+    loadfunction(event);
+  }
+
+}
+// https://testing102.firebaseio.com/annotations.json?uri='http...'
+// https://testing102.firebaseio.com/annotations.json?sortBy="uri"&equalTo='http...'
