63511020ed690471a3e8a346644e982bbd5b8382
abigsmall <abigsmall@users.noreply.github.com>
2015-10-21 20:47:53 -0700

    [Feature] Implement fetching user info from Facebook with access token and cleanup auth.js

--- extension/auth.js
+++ extension/auth.js
@@ -2,8 +2,7 @@ var fetchToken = function() {
   debugger;
 
   var access_token;
-  var clientID = 'CLIENT ID';
-  var clientSecret = 'CLIENT SECRET';
+  var clientID = 'CLIENT_ID';
   var redirectUri = 'https://' + chrome.runtime.id + '.chromiumapp.org/provider_cb';
 
   var options = {
@@ -11,65 +10,50 @@ var fetchToken = function() {
     url: 'https://www.facebook.com/dialog/oauth?client_id=' + clientID + '&response_type=token&access_type=online&redirect_uri=' + encodeURIComponent(redirectUri) 
   }
 
-  // var exchangeCodeForToken = function(code) {
-  //   $.ajax({
-  //     url: 'https://graph.facebook.com/oauth/access_token?client_id=' + clientID +'&client_secret=' + clientSecret +
-  //       '&redirect_uri=' + redirectUri +
-  //       '&code=' + code
-  //   })
-  //   .done(function(response) {
-  //     access_token = response['access_token'];
-  //   });
-  // }
+  chrome.identity.launchWebAuthFlow(options, function(redirectUri) {
+    debugger;
 
-    chrome.identity.launchWebAuthFlow(options, function(redirectUri) {
-      debugger;
-      // console.log(redirectUri);
-      if (chrome.runtime.lastError) {
-        console.log(new Error(chrome.runtime.lastError));
-        return;
-      }
-      
-      var string = redirectUri.slice(redirectUri.indexOf('#')+1);
-      var pairs = string.split('=');
-      var token = pairs[1].split('&');
-      var values = {};
-      values[pairs[0]] = token[0];
-      if (values.hasOwnProperty('access_token')) {
-        access_token = values['access_token'];
-      } 
-      // else if (values.hasOwnProperty('code')) {
-      //   exchangeCodeForToken(values['code']);
-      // }
+    if (chrome.runtime.lastError) {
+      console.log(new Error(chrome.runtime.lastError));
+      return;
+    }
+    
+    var string = redirectUri.slice(redirectUri.indexOf('#')+1);
+    var pairs = string.split('=');
+    var token = pairs[1].split('&');
+    var values = {};
+    values[pairs[0]] = token[0];
+    if (values.hasOwnProperty('access_token')) {
+      access_token = values['access_token'];
+      fetchFbProfile(access_token);
+    }
 
-      chrome.storage.sync.set({'access_token': access_token});
-    });
-
-  
+    chrome.storage.sync.set({'access_token': access_token});
+  });
 }
 
 chrome.storage.sync.clear();
 
 chrome.browserAction.onClicked.addListener(function() {
   chrome.storage.sync.get('access_token', function(obj) {
-    console.log('inside browserAction');
     if (!obj['access_token']) {
       fetchToken();
     }
   });
-
 });
 
-// if (authToken) {
-//   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-//     console.log(tabs[0]);
-//     chrome.tabs.sendMessage(tabs[0].id, {action: "renderComponents"});  
-//   })
-// }
-
-
-
-
-
-
-
+function fetchFbProfile(accessToken) {
+  var xhr = new XMLHttpRequest();
+  var url = 'https://graph.facebook.com/v2.5/me/?fields=id,name,picture&access_token=' + accessToken;
+  xhr.open('GET', url, true);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4) {
+      var resp = JSON.parse(xhr.responseText);
+      var profile = {};
+      profile.id = resp.id,
+      profile.name = resp.name,
+      profile.picUrl = resp.picture.data.url
+    }
+  }
+  xhr.send();
+};
