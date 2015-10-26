27d1bef5c7f7fb4e8644e4dbc92d26884c50757a
abigsmall <abigsmall@users.noreply.github.com>
2015-10-26 16:53:02 -0700

    [Progress] Add console logs to auth.js for easier debugging

--- extension/js/auth.js
+++ extension/js/auth.js
@@ -36,8 +36,11 @@ function fetchToken() {
 chrome.storage.sync.clear();
 
 chrome.browserAction.onClicked.addListener(function() {
+  console.log('browserAction clicked');
   chrome.storage.sync.get('access_token', function(obj) {
+    console.log('browserAction clicked, inside callback to get, obj:', obj);
     if (!obj['access_token']) {
+      console.log('browserAction clicked, inside callback to get, access_token not found');
       fetchToken();
     }
   });
@@ -55,6 +58,9 @@ function fetchFbProfile(accessToken) {
       profile.full_name = resp.name;
       profile.pic_url = resp.picture.data.url;
       profile.email = resp.email;
+      chrome.storage.sync.set({'facebook_id': resp.id}, function() {
+        console.log('facebook_id set in storage');
+      });
       sendFbProfile(profile);
     }
   }
