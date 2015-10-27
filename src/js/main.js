44a6d68029340d066da4c0ecbe36ffb5ddd793e7
abigsmall <abigsmall@users.noreply.github.com>
2015-10-26 17:20:10 -0700

    [Progress] Add console logs and listener for chrome.storage facebook_id\n\nInitial configuration for Annotator takes place in test.js. Specifically, setting the value of app.ident.identity (so that the rest of Annotator has access to it) takes place after app.start(), in the callback to the .then() that follows. Annotator was previously being initiated only at the start of a page and whenever an access_token was retrieved from chrome.storage. However, app.ident.identity should take the value of the Facebook ID that is associated with that token, not the token itself. A second call to Facebook, this time to their Graph API, is required to retrieve that Facebook ID.\n\nAnnotator should thus be initiated after successful retrieval of the Facebook ID and storage of that ID to chrome.storage. Adding identityListener to the onChange event for facebook_id in chrome.storage fulfills said scenario.

--- src/js/main.js
+++ src/js/main.js
@@ -11,7 +11,7 @@ var renderComponents = function() {
 }
 
 var tokenListener = function(changes) {
-  console.log("inside addlistener", changes);
+  console.log("inside tokenlistener (main.js line 14)", changes);
   if (changes.access_token.newValue) {
     renderComponents();
     test.annotate();
@@ -20,10 +20,36 @@ var tokenListener = function(changes) {
 }
 
 chrome.storage.sync.get('access_token', function(obj) {
+  console.log('main.js > chrome storage get token (line 23)');
   if (obj['access_token']) {
+    console.log('main.js > chrome storage get token > if ( access token ) (line 25)');
     renderComponents();
+    console.log('main.js > chrome.storage.sync.get > if ( access token ) (line 27)');
     test.annotate();
   } else {
+    console.log('main.js > chrome.storage.sync.get > else (line 30)');
     chrome.storage.onChanged.addListener(tokenListener);
+  }
+})
+
+var identityListener = function(changes) {
+  console.log("inside identitylistener (main.js line 36)", changes);
+  if (changes.facebook_id.newValue) {
+    renderComponents();
+    test.annotate();
+  }
+  chrome.storage.onChanged.removeListener(identityListener);
+}
+
+chrome.storage.sync.get('facebook_id', function(obj) {
+  console.log('main.js > chrome.storage get fb id (line 45)');
+  if (obj['access_token']) {
+    console.log('main.js > chrome storage get fb id > if ( access token ) (line 47)');
+    renderComponents();
+    console.log('main.js > chrome storage get fb id > if ( access token ) (line 49)');
+    test.annotate();
+  } else {
+    console.log('main.js > chrome storage get fb id > else (line 52)');
+    chrome.storage.onChanged.addListener(identityListener);
   }
 })
