78bdf3a166e7428299fb631911576241b32135f9
Irving <Irvingb232@gmail.com>
2015-10-26 17:19:52 -0700

    [Feature] Header skeleton complete

--- extension/js/auth.js
+++ extension/js/auth.js
@@ -1,7 +1,7 @@
 function fetchToken() {
   var access_token;
 
-  var clientID = 'none';
+  var clientID = '';
 
   var redirectUri = 'https://' + chrome.runtime.id + '.chromiumapp.org/provider_cb';
 
